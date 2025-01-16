24439640e19fa849e95b023c53bed4f36208a299
SuhailMenakuth <suhailmenakuth@gmail.com>
2025-01-16 11:12:11 +0530

    Time: 6 ms (51.31%), Space: 29.1 MB (6.81%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cs
+++ 0441-arranging-coins/0441-arranging-coins.cs
@@ -2,17 +2,16 @@ public class Solution {
     public int ArrangeCoins(int n) {
 
         int i = 1;
-        int remaining = n;
+        // int remaining = n;
         int completed = 0;
         while(i <= n){
-            if (remaining < i){
+            if (n < i){
                 break;
             }
             else{
-                remaining = remaining -i;
+                n = n -i;
                 i++;
                 completed += 1;
-
             }
         }
         return completed;
