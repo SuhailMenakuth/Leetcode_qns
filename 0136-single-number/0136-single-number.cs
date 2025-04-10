e92d4e8086a406bc3386dbbe77b063cce530c735
SuhailMenakuth <suhailmenakuth@gmail.com>
2025-04-10 21:04:35 +0530

    Time: 4 ms (46.57%), Space: 49.4 MB (11.66%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        Dictionary<int , int> numbers = new();
+        foreach(int num in nums){
+           if(numbers.ContainsKey(num))
+           numbers[num]++ ;
+           else
+           numbers[num] = 1;
+        }
+
+        foreach(var pair in numbers){
+            if(pair.Value < 2){
+                return pair.Key;
+            }
+        }
+        return -1 ; 
+    }
+}
